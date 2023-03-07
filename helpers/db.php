109c5437e1a9d73d305d740bc5dd577d880daf18
Noah Abbott <16162162+nhabbott@users.noreply.github.com>
2023-03-07 00:06:08 -0600

    Update db.php

--- helpers/db.php
+++ helpers/db.php
@@ -1,11 +1,11 @@
 <?php
 	define("user", "xxlmm13xx"); // DB Username
-	define("pass", "BW5gu2NMCh"); // DB Password
+	define("pass", "REDACTED"); // DB Password
 	define("db", "xxlmm13xx_tracker"); // DB to use
 	define("host", "xxlmm13xxgaming.com"); // DB Host
 
 	$script = 2361;
-	$api_key = "254bbc439a6fc053455823a6d476cc16";
+	$api_key = "REDACTED";
 	$agent = "Mozilla/5.0 (compatible; AddonTools/1.0; +https://panel.xxlmm13xxgaming.com)";
 	
 	$conn = new PDO('mysql:host='.host.';dbname='.db.';charset=utf8', user, pass, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
